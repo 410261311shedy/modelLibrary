051c839800594e2308cfaa98142946e006de03d5
410261311shedy <m12192002@gmail.com>
2025-12-18 18:15:36 +0800

    landing page query buttoms apearrances

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 //landing page
+'use client';
 import React from 'react'
 import BackgroundBlobs from "../../components/BackgroundBlobs"
 import Image from 'next/image'
-import { Link } from 'lucide-react';
+import {Building, Boxes,Box,FileBox} from 'lucide-react';
 import Footer from '@/components/Footer';
 import ProjectCard from '@/components/cards/ProjectCard';
 const Home = () => {
@@ -14,10 +15,10 @@ const Home = () => {
         <p className="text-[#5B5B5B] dark:text-[#BEBEBE] text-[17px] font-almarai">Build a 3D Model Community--Share Knowledge,Connect Partners</p>
         <div className='flex gap-5  '>
           <a href="/sign-up">  
-            <button className='font-inter  bg-primary text-white text-sm px-[12px] py-[5px] rounded-lg hover:bg-red-500 transition'>Get Started</button>
+            <button className='font-inter  bg-primary text-white text-sm px-4 py-2 rounded-lg hover:bg-red-500 transition'>Get Started</button>
           </a>
           <a href="/explore">
-            <button className='font-inter font-semibold bg-transparent text-[#3C3C3C] dark:text-white text-sm  border-1.5 px-[12px] py-[4px] rounded-lg hover:bg-gray-300 transition'>Explore</button>
+            <button className='font-inter font-semibold bg-transparent text-[#3C3C3C] dark:text-white text-sm  border-1.5 px-4 py-[7px] rounded-lg hover:bg-gray-300 transition'>Explore</button>
           </a>
         </div>
       </div>
@@ -27,15 +28,49 @@ const Home = () => {
       </div>
       
       <div className='flex flex-col items-center gap-6 mt-10 '>
-        <div className='items-center '>
-            Query: "Building | Products | Elements | 2D Drawing"
+        <div className='px-5 flex justify-between gap-5 items-center border-2 w-full border-red-500'>
+          {/*text-transparent bg-gradient-to-r from-primary-400 to-secondary-400 */}
+          <button onClick={() => {}} className='flex flex-col justify-center items-center gap-2 border-1 border-[#B8B8B8] rounded-[8px] h-[120px] w-[200px] hover:cursor-pointer font-abeezee text-sm text-[#B8B8B8]'>
+              <Building 
+                height={40}
+                width={40}
+                className=''
+                />
+              <p>Buildings</p>
+          </button>
+          <button onClick={() => {}} className='flex flex-col justify-center items-center gap-2 border-1 border-[#B8B8B8] rounded-[8px] h-[120px] w-[200px] hover:cursor-pointer font-abeezee text-sm text-[#B8B8B8]'>
+              <Boxes 
+                height={40}
+                width={40}
+                className=''
+                />
+              <p>Products</p>
+          </button>
+          <button onClick={() => {}} className='flex flex-col justify-center items-center gap-2 border-1 border-[#B8B8B8] rounded-[8px] h-[120px] w-[200px] hover:cursor-pointer font-abeezee text-sm text-[#B8B8B8]'>
+              <Box
+                height={40}
+                width={40}
+                className=''
+                />
+              <p>Elements</p>
+          </button>
+          <button onClick={() => {}} className='flex flex-col justify-center items-center gap-2 border-1 border-[#B8B8B8] rounded-[8px] h-[120px] w-[200px] hover:cursor-pointer font-abeezee text-sm text-[#B8B8B8]'>
+              <FileBox 
+                height={40}
+                width={40}
+                className=''
+                />
+              <p>2D Drawings</p>
+          </button>
         </div>
           <div>
             Query: "Newest | Hottest"
           </div>
           <div>
-            <div>
+            <div className='flex gap-3'>
               {/* Display area for model cards, reder 12 when first mounted */}
+              <ProjectCard/>
+              <ProjectCard/>
               <ProjectCard/>
             </div>  
             <div className='flex justify-center mt-4 mb-4 '>
--- components/cards/ProjectCard.tsx
+++ components/cards/ProjectCard.tsx
@@ -5,12 +5,12 @@ import Image from 'next/image';
 
 const ProjectCard = () => {
   return (
-    <Card className="flex-col py-4 bg-[#FFFFF4] dark:bg-[#3F3F46] w-[300px] h-[260px] border-none shadow-[0px_5px_5px_rgba(0,0,0,0.6)]">
+    <Card className="flex-col pb-4 pt-4 pl-4 pr-4 bg-[#FFFFF4] dark:bg-[#3F3F46] w-[300px] h-[260px] shadow-[4px_4px_20px_rgba(0,0,0,0.32)] dark:shadow-[4px_4px_20px_rgba(0,0,0,0.66),inset_0px_2px_5px_rgba(255,255,255,0.25)]">
       <CardBody 
-        className="px-4 py-0 relative items-center">
+        className="py-0 px-0 rounded-[20px] relative items-center w-full shadow-[0px_3px_5px_1px_rgba(255,255,255,0.16),0px_-2px_2.5px_rgba(0,0,0,0.25)]">
           
         {/* Image縮放的裁剪框 */}
-        <div className='relative w-full h-[230px] overflow-hidden rounded-[20px] group bg-[#FFFFF4] dark:bg-[#18181B]'>  
+        <div className='relative w-full h-[230px] overflow-hidden rounded-[20px] group bg-[#FFFFF4] dark:bg-[#18181B] '>  
           
           <Image
             className="object-cover w-full h-full transition-transform duration-300 scale-110 hover:scale-140"
@@ -20,8 +20,7 @@ const ProjectCard = () => {
             height={230}
           />
           {/* 內凹陰影覆蓋層 */}
-          <div className="absolute inset-0 rounded-[20px] pointer-events-none z-10 
-                      shadow-[inset_0px_5px_5px_rgba(0,0,0,0.6),inset_0px_-1px_10px_rgba(255,255,245,0.4)]">
+          <div className="absolute rounded-[20px] pointer-events-none z-10 inset-0">
           </div>
           {/* 右上角的懸浮小圖示 */}
           <div className="bg-[#FFFFF4] dark:bg-[#3F3F46] absolute top-3 right-5 p-2 rounded-full backdrop-blur-md z-20 border border-white/10">
